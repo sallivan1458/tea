4822cf2f29cefaf0f7e3d0be146391bcc7d9caa3
Sallivan <Sallivan1458@yandex.ru>
2026-01-08 20:54:35 +0500

    base url /

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
 import * as path from 'path';
+import { fileURLToPath } from 'url'
 
-const repoName = 'tea';
+// const repoName = 'tea';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-    base: `/${repoName}/`,
+    base: `/`,
     plugins: [
         react(),
         svgr(),
